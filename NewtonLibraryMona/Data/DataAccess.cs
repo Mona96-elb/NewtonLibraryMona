9807d8f2e820cb63b20551b367bb1cb4bdd5f6fe
Mona_ <Mona_961108@hotmail.com>
2023-12-04 10:13:47 +0100

    Add project files.

---
+++ NewtonLibraryMona/Data/DataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonLibraryMona.Data
+{
+    internal class DataAccess
+    {
+    }
+}
---
+++ NewtonLibraryMona/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NewtonLibraryMona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
